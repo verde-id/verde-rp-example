32f88e4f4e6ab6024ebf12b920a3b8999c37fee3
petch <tanee.kumpijit@gmail.com>
2018-02-24 16:11:56 +0700

    can start individually

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import 'source-map-support/register';
+import fs from 'fs'
 
-import './greenBoxApi';
-import './web/server';
+if(!process.env.CONFIG_FILE) throw 'Please specify config file';
+let config = JSON.parse(fs.readFileSync(process.env.CONFIG_FILE).toString());
+process.env.REQUESTS_CONTRACT_ADDR = config.contractAddress.request; 
+process.env.DIRECTORY_CONTRACT_ADDR = config.contractAddress.directory;
+process.env.RP_ADDR = config.partyAddress[9];
+
+require('source-map-support/register');
+
+require('./greenBoxApi');
+require('./web/server');
